4b9319927a5f6d6d8f14f957b3b5e8d720db3251
cong <ntcong1997@gmail.com>
2022-12-04 11:47:01 +0700

    feat(test): test track lyrics

---
+++ domain/src/test/java/com/example/domain/model/TrackTest.kt
@@ -0,0 +1,162 @@
+package com.example.domain.model
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Created by TC on 04/12/2022.
+ */
+
+class TrackTest {
+    @Test
+    fun track_has_lyrics() {
+        val track = Track(
+            alias = null,
+            artists = listOf(),
+            genres = Genres("POP"),
+            hub = Hub(
+                actions = listOf(
+                    Action(
+                        id = "123",
+                        name = "apple",
+                        type = null,
+                        uri = "https://..."
+                    )
+                ),
+                displayname = null,
+                explicit = null,
+                image = null,
+                options = null,
+                type = null
+            ),
+            images = Images(
+                background = null,
+                coverart = null,
+                coverarthq = null
+            ),
+            key = "1",
+            layout = null,
+            releasedate = null,
+            sections = listOf(
+                Section(
+                    beacondata = null,
+                    footer = null,
+                    metadata = null,
+                    metapages = null,
+                    tabname = null,
+                    text = listOf("A", "B", "C"),
+                    type = "LYRICS"
+                )
+            ),
+            share = null,
+            subtitle = "One Direction",
+            title = "Story of my life",
+            type = "MUSIC",
+            url = "https://...",
+            urlparams = null
+        )
+
+        val expectedResult = listOf("A", "B", "C")
+        val actualResult = track.lyrics
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun track_has_no_lyrics_sections_is_null() {
+        val track = Track(
+            alias = null,
+            artists = listOf(),
+            genres = Genres("POP"),
+            hub = Hub(
+                actions = listOf(
+                    Action(
+                        id = "123",
+                        name = "apple",
+                        type = null,
+                        uri = "https://..."
+                    )
+                ),
+                displayname = null,
+                explicit = null,
+                image = null,
+                options = null,
+                type = null
+            ),
+            images = Images(
+                background = null,
+                coverart = null,
+                coverarthq = null
+            ),
+            key = "1",
+            layout = null,
+            releasedate = null,
+            sections = null,
+            share = null,
+            subtitle = "One Direction",
+            title = "Story of my life",
+            type = "MUSIC",
+            url = "https://...",
+            urlparams = null
+        )
+
+        val expectedResult = listOf<String>()
+        val actualResult = track.lyrics
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun track_has_no_lyrics_sections_have_no_element_lyrics() {
+        val track = Track(
+            alias = null,
+            artists = listOf(),
+            genres = Genres("POP"),
+            hub = Hub(
+                actions = listOf(
+                    Action(
+                        id = "123",
+                        name = "apple",
+                        type = null,
+                        uri = "https://..."
+                    )
+                ),
+                displayname = null,
+                explicit = null,
+                image = null,
+                options = null,
+                type = null
+            ),
+            images = Images(
+                background = null,
+                coverart = null,
+                coverarthq = null
+            ),
+            key = "1",
+            layout = null,
+            releasedate = null,
+            sections = listOf(
+                Section(
+                    beacondata = null,
+                    footer = null,
+                    metadata = null,
+                    metapages = null,
+                    tabname = null,
+                    text = null,
+                    type = "SONG"
+                )
+            ),
+            share = null,
+            subtitle = "One Direction",
+            title = "Story of my life",
+            type = "MUSIC",
+            url = "https://...",
+            urlparams = null
+        )
+
+        val expectedResult = listOf<String>()
+        val actualResult = track.lyrics
+
+        assertEquals(expectedResult, actualResult)
+    }
+}
